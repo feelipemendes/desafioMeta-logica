31a779b23dabb010dc50cd4c502ad103df134d8a
feelipemendes <felipe14_mendes@hotmail.com>
2019-10-12 11:58:25 -0300

    QUESTAO 2 - OK

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] v = { "{", "[", "(", ")", "]", "}" };
+            var lastIndex = v.Length - 1;
+
+
+
+            for (int i = 0; i < (v.Length / 2); i++)
+            {
+                if (v[i] == "(")
+                {
+                    if (v[lastIndex - i] == ")")
+                    {
+                        continue;
+                    }
+                }
+
+                if (v[i] == "[")
+                {
+                    if (v[lastIndex - i] == "]")
+                    {
+                        continue;
+                    }
+                }
+
+                if (v[i] == "{")
+                {
+                    if (v[lastIndex - i] == "}")
+                    {
+                        continue;
+                    }
+                }
+
+                //NAO BALANCEADO
+                Console.WriteLine("NAO!");
+                Console.ReadKey();
+
+                return;
+            }
+
+            //BALANCEADO
+            Console.WriteLine("SIM!");
+            Console.ReadKey();
+        }
+    }
+}
